c3e26a3650e3e06acb81b75bac3b0f805efcbab0
iviiincent <ishongma@outlook.com>
2024-03-27 13:29:47 +0800

    finished method emptySpaceExists

--- proj0/game2048/Model.java
+++ proj0/game2048/Model.java
@@ -7,7 +7,7 @@ import java.util.Observable;
 /**
  * The state of a game of 2048.
  *
- * @author TODO: YOUR NAME HERE
+ * @author iviiincent
  */
 public class Model extends Observable {
     /**
@@ -172,6 +172,13 @@ public class Model extends Observable {
      */
     public static boolean emptySpaceExists(Board b) {
         // TODO: Fill in this function.
+        for (int i = 0; i < b.size(); i++) {
+            for (int j = 0; j < b.size(); j++) {
+                if (b.tile(i, j) == null) {
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
